03b8e4c2537108d8a70423261f3d0b2b203f86c4
Kothwalshaharin <93109454+Kothwalshaharin@users.noreply.github.com>
2022-01-10 18:23:22 +0530

    Update p0original.c

--- p0original.c
+++ p0original.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main ()
 {
-  char name[20]
+  char name[20];
    printf("enter your name\n");
-   scanf("%s\n",name);
-  printf("%s\n",name):
+   scanf("%s",name);
+  printf("%s\n",name);
   return 0;
 }
